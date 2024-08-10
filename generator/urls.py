b3d58fae2f882fcb3dc42a083048fde65869d9bb
nadprime <nadprime05@gmail.com>
2024-08-10 13:15:24 +0000

    added templates

--- generator/urls.py
+++ generator/urls.py
@@ -4,4 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('generator/', views.index, name='index'),
+    path('suggestions/', views.ai_suggestions, name='ai_suggestions'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('calender/', views.content_calender, name='content_calender'),
+    path('content/', views.create_content, name='create_content'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('help/', views.help_center, name='help_center'),
+    path('insights/', views.monetization_insights, name='monetization_insights'),
+    path('feedback/', views.real_time_feedback, name='real_time_feedback'),
+    path('settings/', views.settings, name='settings'),
+    path('collaboration/', views.team_collaboration, name='team_collaboration'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -7,3 +7,31 @@ def index(request):
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+def ai_suggestions(request):
+    return render(request, 'ai_suggestions.html')
+
+def analytics(request):
+    return render(request, 'analytics.html')
+
+def content_calender(request):
+    return render(request, 'content_calendar.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def help_center(request):
+    return render(request, 'help_center.html')
+
+def monetization_insights(request):
+    return render(request, 'monetization_insights.html')
+
+def real_time_feedback(request):
+    return render(request, 'real_time_feedback.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def team_collaboration(request):
+    return render(request, 'team_collaboration.html')
+
